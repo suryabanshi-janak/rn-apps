84d8da171ed8ee99ec62c3ef3691c077c496d4a0
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-11-05 22:19:32 +0545

    feat: setting font and styles

--- aora/app/index.tsx
+++ aora/app/index.tsx
@@ -1,21 +1,14 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 const app = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='font-pbold text-2xl'>Aora!</Text>
+      <Link href='/profile'>Go to profile</Link>
       <StatusBar style='auto' />
     </View>
   );
 };
 export default app;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-});
