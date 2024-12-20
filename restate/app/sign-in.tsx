19af5ddaeeab886c7e3d75f93e57032e0aa01692
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-12-20 23:08:32 +0545

    restate: signin page

--- restate/app/sign-in.tsx
+++ restate/app/sign-in.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import images from '@/constants/images';
+import icons from '@/constants/icons';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='h-full bg-white'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image
+          source={images.onboarding}
+          className='w-full h-4/6'
+          resizeMode='contain'
+        />
+
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>
+            Welcome to Restate
+          </Text>
+
+          <Text className='mt-2 text-3xl text-center text-black-300 font-rubik-bold'>
+            Let's Get You Closer to {'\n'}
+            <Text className='text-primary-300'>Your Ideal Home</Text>
+          </Text>
+
+          <Text className='mt-12 text-lg text-center font-rubik text-black-200'>
+            Login to Restate with Google
+          </Text>
+
+          <TouchableOpacity className='w-full py-4 mt-5 bg-white border border-gray-100 rounded-full shadow-md shadow-zinc-300'>
+            <View className='flex flex-row items-center justify-center'>
+              <Image
+                source={icons.google}
+                className='w-5 h-5'
+                resizeMode='contain'
+              />
+              <Text className='mt-1 ml-2 text-lg font-rubik-medium text-black-300'>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
